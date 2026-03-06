55aeee8b5f18e2c0a45b164d772326f0377bb8ac
NguyenTrongHieutt <nguyentronghieu2kk5@gmail.com>
2026-03-06 09:09:13 +0700

    Buoi2_version2

--- Thuc_hanh_Buoi2/app.py
+++ Thuc_hanh_Buoi2/app.py
@@ -1,13 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return jsonify({
-        "message": "Hello from REST API",
-        "principle": "Client-Server"
-    })
+users = [
+    {"id": 1, "name": "Alice"},
+    {"id": 2, "name": "Bob"}
+]
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    return jsonify(users)
+
+@app.route("/users/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+    for u in users:
+        if u["id"] == user_id:
+            return jsonify(u)
+    return {"error": "User not found"}, 404
+
+@app.route("/users", methods=["POST"])
+def add_user():
+    data = request.json
+    users.append(data)
+    return jsonify(data), 201
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
